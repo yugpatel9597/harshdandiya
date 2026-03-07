e03d08df4d0463435698c973ea4bfb7816dc01b2
yugpatel9597 <131437656+yugpatel9597@users.noreply.github.com>
2026-03-07 19:24:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,11 +31,12 @@ if (!fs.existsSync(uploadsDir)) {
 
 // Middleware
 app.use(cors({
-    origin: function (origin, callback) {
-        callback(null, true);
-    },
-    credentials: true,
+  origin: "http://localhost:5173",
+  methods: ["GET","POST","PUT","DELETE","PATCH"],
+  credentials: true
 }));
+
+app.options("*", cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
