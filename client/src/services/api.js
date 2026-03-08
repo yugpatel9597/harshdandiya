5d314e16ac55cb458b1d33073c3d1212c6497993
yugpatel9597 <131437656+yugpatel9597@users.noreply.github.com>
2026-03-08 10:57:38 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://dandiyakart-server.onrender.com/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://harshdandiya.onrender.com/api',
     headers: { 'Content-Type': 'application/json' },
 });
 
